3af0f89c2c8925da7ed9ebb635dd255cb4ac6a28
tiaL-ops <104815994+tiaL-ops@users.noreply.github.com>
2024-12-07 19:38:28 -0500

    CLeaning code in process

--- src/main/java/com/example/ExpenseController.java
+++ src/main/java/com/example/ExpenseController.java
@@ -1,65 +1,59 @@
 package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import javax.validation.Valid;
 import java.util.List;
 import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://127.0.0.1:5173")
 public class ExpenseController {
 
     private final ExpenseTracker expenseTracker;
+    private final TransactionRepository transactionRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    private TransactionRepository transactionRepository;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    public ExpenseController(ExpenseTracker expenseTracker, TransactionRepository transactionRepository) {
+    public ExpenseController(ExpenseTracker expenseTracker, TransactionRepository transactionRepository, UserRepository userRepository) {
         this.expenseTracker = expenseTracker;
         this.transactionRepository = transactionRepository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/test")
-    public String testEndpoint() {
-        return "Backend working";
+    public ResponseEntity<String> testEndpoint() {
+        return ResponseEntity.ok("Backend working");
     }
 
-    @RequestMapping(value = "/transactions", method = RequestMethod.POST)
-public void createTransaction(@RequestParam User user, @RequestBody Transaction transaction) {
-    if ("income".equalsIgnoreCase(transaction.getType())) {
-        expenseTracker.addIncome(transaction.getDate(), transaction.getAmount(), transaction.getCategory(),transaction.getUserId());
-    } else {
-        expenseTracker.addExpense(transaction.getDate(), transaction.getAmount(), transaction.getCategory(),transaction.getUserId());
+    @PostMapping("/transactions")
+    public ResponseEntity<String> createTransaction(@RequestBody @Valid Transaction transaction) {
+        if ("income".equalsIgnoreCase(transaction.getType())) {
+            expenseTracker.addIncome(transaction.getDate(), transaction.getAmount(), transaction.getCategory(), transaction.getUserId());
+        } else {
+            expenseTracker.addExpense(transaction.getDate(), transaction.getAmount(), transaction.getCategory(), transaction.getUserId());
+        }
+        return ResponseEntity.status(201).body("Transaction created successfully");
     }
-}
+
     @GetMapping("/transactions")
-    public List<Transaction> getTransactions() {
-        return expenseTracker.getTransactions();
+    public ResponseEntity<List<Transaction>> getTransactions() {
+        List<Transaction> transactions = expenseTracker.getTransactions();
+        return ResponseEntity.ok(transactions);
     }
 
-    @GetMapping("/test-direct-save")
-    public String testDirectSave() {
-        System.out.println("Endpoint invoked!");
+    @PostMapping("/test-direct-save")
+    public ResponseEntity<String> testDirectSave() {
         User user = new User();
         user.setUsername("helloTest1");
         user.setEmail("test1@gmail.com");
         user.setPassword("passWord");
-    
-       
         userRepository.save(user);
-    
-        
+
         Transaction transaction = new Transaction(LocalDate.now(), 100.0, "test_category", "income", user);
-    
-      
         transactionRepository.save(transaction);
-        return "Direct transaction saved!";
+
+        return ResponseEntity.status(201).body("Direct transaction saved!");
     }
-    
 }
